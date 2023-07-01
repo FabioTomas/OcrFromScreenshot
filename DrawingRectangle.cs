b43e67f3ea7412a436d53eb1ac76082fb63bd590
Fabio Tomas de Aquino <fabio.aquino@animaeducacao.com.br>
2023-07-01 10:55:57 -0300

    Tesseract inclusion.

--- DrawingRectangle.cs
+++ DrawingRectangle.cs
@@ -5,7 +5,7 @@
         public Rectangle Rect => new Rectangle(Location, Size);
         public Size Size { get; set; }
         public Point Location { get; set; }
-        public Control Owner { get; set; }
+        public Control? Owner { get; set; }
         public Point StartPosition { get; set; }
         public Color DrawingcColor { get; set; } = Color.LightGreen;
         public float PenSize { get; set; } = 3f;
